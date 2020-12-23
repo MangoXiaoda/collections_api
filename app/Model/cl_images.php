1c84b9d1bdb6bc304ddf20ab6ee2d3893fff2975
lzd <mangoxiaoda@gmail.com>
2020-12-23 11:27:17 +0000

    调整图片表模型文件

---
+++ app/Model/cl_images.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class cl_images extends Model
+{
+    protected $table = 'cl_images';
+    protected $primaryKey = 'id';
+
+    public $timestamps = true;
+}
